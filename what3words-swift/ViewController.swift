8de795bb4cd5c57731ae126b68c70eebf5dbfe6c
yodaaa <01ewell0@gmail.com>
2018-11-13 15:58:51 +0900

    Initial Commit

---
+++ what3words-swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  what3words-swift
+//
+//  Created by yodaaa on 2018/11/13.
+//  Copyright © 2018 yodaaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ what3words-swiftTests/what3words_swiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  what3words_swiftTests.swift
+//  what3words-swiftTests
+//
+//  Created by yodaaa on 2018/11/13.
+//  Copyright © 2018 yodaaa. All rights reserved.
+//
+
+import XCTest
+@testable import what3words_swift
+
+class what3words_swiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
